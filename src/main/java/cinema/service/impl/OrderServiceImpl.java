5fe4deccee44163456aef529228e698c0ed6b3a4
djmolot <djmolot@gmail.com>
2022-07-21 18:32:44 +0200

    created cinema app using spring

--- src/main/java/cinema/service/impl/OrderServiceImpl.java
+++ src/main/java/cinema/service/impl/OrderServiceImpl.java
@@ -1,38 +1,38 @@
 package cinema.service.impl;
 
 import cinema.dao.OrderDao;
-import cinema.lib.Inject;
-import cinema.lib.Service;
 import cinema.model.Order;
 import cinema.model.ShoppingCart;
-import cinema.model.Ticket;
 import cinema.model.User;
 import cinema.service.OrderService;
 import cinema.service.ShoppingCartService;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-    @Inject
-    private ShoppingCartService shoppingCartService;
-    @Inject
-    private OrderDao orderDao;
+    private final OrderDao orderDao;
+    private final ShoppingCartService shoppingCartService;
+
+    public OrderServiceImpl(OrderDao orderDao, ShoppingCartService shoppingCartService) {
+        this.orderDao = orderDao;
+        this.shoppingCartService = shoppingCartService;
+    }
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
-        List<Ticket> ticketsCopy = new ArrayList<>(shoppingCart.getTickets());
         Order order = new Order();
-        order.setTickets(ticketsCopy);
+        order.setOrderTime(LocalDateTime.now());
+        order.setTickets(shoppingCart.getTickets());
         order.setUser(shoppingCart.getUser());
-        order.setOrderDate(LocalDateTime.now());
-        shoppingCartService.clearShoppingCart(shoppingCart);
-        return orderDao.add(order);
+        orderDao.add(order);
+        shoppingCartService.clear(shoppingCart);
+        return order;
     }
 
     @Override
     public List<Order> getOrdersHistory(User user) {
-        return orderDao.getByUser(user);
+        return orderDao.getOrdersHistory(user);
     }
 }
